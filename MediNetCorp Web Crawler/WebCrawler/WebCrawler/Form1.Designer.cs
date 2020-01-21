84e4fa9af08c59d423cb6dc6ac0c055351c3e08b
Amir Mursal <amir.mursal@communitybrands.com>
2020-01-21 20:54:46 +0530

    commit

---
+++ MediNetCorp Web Crawler/WebCrawler/WebCrawler/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WebCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbstate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtkeyword = new System.Windows.Forms.TextBox();
+            this.btngraburl = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmblink = new System.Windows.Forms.ComboBox();
+            this.cmbjob = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtkeyword2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select State :";
+            // 
+            // cmbstate
+            // 
+            this.cmbstate.FormattingEnabled = true;
+            this.cmbstate.Items.AddRange(new object[] {
+            "Alabama",
+            "Alaska",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "District of Columbia",
+            "Florida",
+            "Georgia",
+            "Hawaii",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "Montana",
+            "Nebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming",
+            "Territories"});
+            this.cmbstate.Location = new System.Drawing.Point(200, 43);
+            this.cmbstate.Name = "cmbstate";
+            this.cmbstate.Size = new System.Drawing.Size(121, 21);
+            this.cmbstate.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " Keyword-1 :";
+            // 
+            // txtkeyword
+            // 
+            this.txtkeyword.Location = new System.Drawing.Point(200, 164);
+            this.txtkeyword.Name = "txtkeyword";
+            this.txtkeyword.Size = new System.Drawing.Size(171, 20);
+            this.txtkeyword.TabIndex = 4;
+            // 
+            // btngraburl
+            // 
+            this.btngraburl.Location = new System.Drawing.Point(90, 237);
+            this.btngraburl.Name = "btngraburl";
+            this.btngraburl.Size = new System.Drawing.Size(75, 32);
+            this.btngraburl.TabIndex = 6;
+            this.btngraburl.Text = "Search";
+            this.btngraburl.UseVisualStyleBackColor = true;
+            this.btngraburl.Click += new System.EventHandler(this.btngraburl_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.Location = new System.Drawing.Point(224, 237);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(75, 32);
+            this.btnclose.TabIndex = 7;
+            this.btnclose.Text = "Close";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Select Link Type :";
+            // 
+            // cmblink
+            // 
+            this.cmblink.FormattingEnabled = true;
+            this.cmblink.Items.AddRange(new object[] {
+            "Full Link",
+            "Half Link"});
+            this.cmblink.Location = new System.Drawing.Point(200, 122);
+            this.cmblink.Name = "cmblink";
+            this.cmblink.Size = new System.Drawing.Size(121, 21);
+            this.cmblink.TabIndex = 3;
+            // 
+            // cmbjob
+            // 
+            this.cmbjob.FormattingEnabled = true;
+            this.cmbjob.Items.AddRange(new object[] {
+            "All Jobs",
+            "Medical + Health"});
+            this.cmbjob.Location = new System.Drawing.Point(200, 85);
+            this.cmbjob.Name = "cmbjob";
+            this.cmbjob.Size = new System.Drawing.Size(121, 21);
+            this.cmbjob.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Select Job Type :";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(25, 275);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(346, 23);
+            this.progressBar.TabIndex = 14;
+            this.progressBar.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Keyword-2 :";
+            // 
+            // txtkeyword2
+            // 
+            this.txtkeyword2.Location = new System.Drawing.Point(200, 193);
+            this.txtkeyword2.Name = "txtkeyword2";
+            this.txtkeyword2.Size = new System.Drawing.Size(171, 20);
+            this.txtkeyword2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 321);
+            this.Controls.Add(this.txtkeyword2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbjob);
+            this.Controls.Add(this.cmblink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btngraburl);
+            this.Controls.Add(this.txtkeyword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbstate);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MedineT Corp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbstate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtkeyword;
+        private System.Windows.Forms.Button btngraburl;
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmblink;
+        private System.Windows.Forms.ComboBox cmbjob;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtkeyword2;
+    }
+}
+
